972597fc5bd329a56bab30309f8dc256585b019c
Abhishek Sarkar <abhisheksarkar444@gmail.com>
2021-03-17 01:13:06 +0530

    Slack clone base setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDn8R0X7B4ai0YdYNc0pTZ67mB0RJUdHDM",
+  authDomain: "slack-clone-3cef5.firebaseapp.com",
+  projectId: "slack-clone-3cef5",
+  storageBucket: "slack-clone-3cef5.appspot.com",
+  messagingSenderId: "483189341406",
+  appId: "1:483189341406:web:008ad6d110e6447d02964f",
+};
