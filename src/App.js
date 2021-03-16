981106aab64a55ac55588e4473fe60f3a079230b
Abhishek Sarkar <abhisheksarkar444@gmail.com>
2021-03-17 01:32:42 +0530

    router configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build Slack</h1>
+      <Router>
+        <>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/" exact>
+              <h1>This is the homepage..!! </h1>
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
